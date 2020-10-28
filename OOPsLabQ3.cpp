b71a6aca5c122aa652d4522385a74376eaf58758
Mudit Sharma <muditsharma23702@gmail.com>
2020-10-28 21:38:12 +0530

    Add files via upload

---
+++ OOPsLabQ3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+inline int CheckLargest(int num1, int num2, int num3)
+{
+    return (num1 > num2) ? ((num1 > num3) ? num1 : num3) : ((num2 > num3) ? num2 : num3);
+}
+int main()
+{
+    int num1, num2, num3;
+    cin >> num1;
+    cin >> num2;
+    cin >> num3;
+    cout<<CheckLargest(num1, num2, num3);
+}
